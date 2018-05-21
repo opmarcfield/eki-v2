03ea2358c5edcd14e56ecfb3eb4dabf5ea728e03
opmarcfield <39411656+opmarcfield@users.noreply.github.com>
2018-05-21 17:50:35 +0300

    Update eki.js

--- eki.js
+++ eki.js
@@ -39,12 +39,7 @@ client.on("message", (message) => {
     message.channel.send(responseObject[message.content]);
   }
 });
-// client.on("message", (message) => {
-//  if (message.content.startsWith(prefix + " add") || message.member.roles.find("name", "Admin")) {
-//    message.channel.send("XP adding system not functional yet");
-// } else message.channel.send("Your rank doesnt allow that ");
-//  }
-// });
+
 client.on("message", (message) => {
   // this happens when a message is received
   if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -73,6 +68,51 @@ client.on("message", (message) => {
   }
 });
 
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+       if(!message.member.roles.some(r=>["Admin"].includes(r.name)) )
+      return message.reply("I'm terribly sorry, but your rank is too low");
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
 
+  if(command === "add") {
+        if(!message.member.roles.some(r=>["Admin"].includes(r.name)) )
+      return message.reply("I'm terribly sorry, but your rank is too low");
+
+    message.channel.send("XP system development in process");
+  }
+});
 
 client.login(process.env.TOKEN);
