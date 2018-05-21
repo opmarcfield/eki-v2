23410795a86565905a5b9d128f2468510b408ace
opmarcfield <39411656+opmarcfield@users.noreply.github.com>
2018-05-21 12:13:14 +0300

    Update eki.js

--- eki.js
+++ eki.js
@@ -21,15 +21,27 @@ client.on("guildDelete", guild => {
 
 const prefix = "eki"
 client.on("message", (message) => {
-  // this happens when a message is received 
+  // this happens when a message is received
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  
+
   if (message.content.startsWith(prefix + "help")) {
     message.channel.send("Beta-eki täs moro. En osaa viel oikein mitään. Admin @Opa osaa mua vähän komentaa, mut muiden komennot rajottuu aika pieniin. 'Ekikomennot' for more info");
   } else
 
   if (message.content.startsWith(prefix + "komennot")) {
     message.channel.send("Meikän komennot: ekihauku, ekikehu, ekiauta, ekisoita");
+  }
+  if (message.content.startsWith(prefix + "hauku")) {
+    message.channel.send("Vittu mikä pelle! Hommaa muija");
+  }
+  if (message.content.startsWith(prefix + "kehu")) {
+    message.channel.send("Vittu mikä sonni! Hyvä ja komee rikas tyyppi!");
+  }
+  if (message.content.startsWith(prefix + "auta")) {
+    message.channel.send("Valitetavasti en osaa auttaa");
+  }
+  if (message.content.startsWith(prefix + "soita")) {
+    message.channel.send("Valitetavasti en osaa soittaa muuta kun KPOP tällä hetkellä");
   }
 });
 
