66e226893516c3fda3b4b7965e83c5b53cb514fb
opmarcfield <39411656+opmarcfield@users.noreply.github.com>
2018-05-25 13:53:22 +0300

    Update eki.js

--- eki.js
+++ eki.js
@@ -41,6 +41,14 @@ client.on("message", (message) => {
   }
 });
 
+client.on("message", message => {
+  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+  prefix = prefixMention.match(message.content) ? message.content.match(prefixMention)[0] + " " : prefix;
+
+  message.channel.send("Hä? Eki täs. 'ekihelp' jos on jotai. Tai pistä DM (älybotti in the making)");
+  }
+});          
+
 client.on("message", (message) => {
   // If the message is "what is my avatar"
   if (message.content === 'What is my avatar') {
